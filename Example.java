fd67cf0ef46b3b49dd49ef378c901dd1dafed8a2
shivaprasad018 <shivamani.vellanki2006@gmail.com>
2025-12-01 11:14:59 +0530

    change in Example file

--- Example.java
+++ Example.java
@@ -3,6 +3,6 @@ Class Example
 	public static void main (String[]args)
 	{
 		System.out.println("Welcome To SNB1 Batch");
-		System.out.println("Day 1: Practice git commands);
+		System.out.println("Day 1: Practice git commands");
 	}
 }
